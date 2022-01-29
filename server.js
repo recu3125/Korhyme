b593c0978432d246d6c25ee58ef9c5eaa0cd4de5
redstonecube <superhoy0509@gmail.com>
2022-01-29 23:00:56 +0900

    8080prt

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const Hangul = require('hangul-js');
 
 var file = [[],[],[]]
 // 3000 포트로 서버 오픈
-var port = 80
+var port = 8080
 app.listen(port, async function () {
   console.log("start at " + port)
   file[0] = await getfile(0)
