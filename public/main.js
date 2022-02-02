d69232cf6969e71d829cd814453eca1d0d15c2c0
redstonecube <superhoy0509@gmail.com>
2022-02-02 13:42:24 +0900

    smooth scroll

--- public/main.js
+++ public/main.js
@@ -76,8 +76,7 @@ function scrollFunction() {
   }
 }
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  pos = 0
 }
 function selectorchange() {
   var selectorarr = [
@@ -235,4 +234,73 @@ function databasemaker(filename) {
   var oFrame = document.getElementById(filename);
   var strRawContents = oFrame.contentWindow.document.body.childNodes[0].innerHTML;
   console.log(stdpron(strRawContents.replace(/\n/g, 'A')).replace(/A/g, '\n').replace(/^L/gm, '').replace(/L$/gm, ''))
+}
+
+function init() {
+  new SmoothScroll(document, 120, 12)
+}
+
+
+
+
+function SmoothScroll(target, speed, smooth) {
+  if (target === document)
+    target = (document.scrollingElement
+      || document.documentElement
+      || document.body.parentNode
+      || document.body) // cross browser support for document scrolling
+
+  var moving = false
+  pos = target.scrollTop
+  var frame = target === document.body
+    && document.documentElement
+    ? document.documentElement
+    : target // safari is the new IE
+  target.addEventListener('mousewheel', scrolled, { passive: false })
+  target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
+  function scrolled(e) {
+    e.preventDefault(); // disable default scrolling
+
+    var delta = normalizeWheelDelta(e)
+
+    pos += -delta * speed
+    pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)) // limit scrolling
+    if (!moving) update()
+  }
+
+  function normalizeWheelDelta(e) {
+    if (e.detail) {
+      if (e.wheelDelta)
+        return e.wheelDelta / e.detail / 40 * (e.detail > 0 ? 1 : -1) // Opera
+      else
+        return -e.detail / 3 // Firefox
+    } else
+      return e.wheelDelta / 120 // IE,Safari,Chrome
+  }
+
+  function update() {
+    moving = true
+
+    var delta = (pos - target.scrollTop) / smooth
+
+    target.scrollTop += delta
+
+    if (Math.abs(delta) > 0.5)
+      requestFrame(update)
+    else
+      moving = false
+  }
+
+  var requestFrame = function () { // requestAnimationFrame cross browser
+    return (
+      window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame ||
+      window.oRequestAnimationFrame ||
+      window.msRequestAnimationFrame ||
+      function (func) {
+        window.setTimeout(func, 1000 / 50);
+      }
+    );
+  }()
 }
